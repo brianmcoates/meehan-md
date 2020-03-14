338c36ffe57ad59f54c6e7d11e659ccd7f96b040
Brian Coates <brian.coates@life.church>
2020-03-14 13:26:02 -0500

    add share this to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -26,7 +26,7 @@ export const BlogPostTemplate = ({
               {title}
             </h1>
             <p>{description}</p>
-            <div className="sharethis-inline-share-buttons"></div>
+            <div className="m-b-1 m-t-1 sharethis-inline-share-buttons"></div>
             <PostContent content={content} />
             {tags && tags.length ? (
               <div style={{ marginTop: `4rem` }}>
