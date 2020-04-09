9bea495861aee3a4ff07b5487d7b70ced7226539
Brian Coates <brian.coates@life.church>
2020-04-09 07:22:51 -0500

    add resources to site

---
+++ src/cms/preview-templates/ResourcesPagePreview.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ResourcesPageTemplate } from '../../templates/resources-page'
+
+const ResourcesPagePreview = ({ entry, getAsset }) => {
+  const entryBooks = entry.getIn(['data', 'intro', 'books']);
+  const books = entryBooks ? entryBooks.toJS() : [];
+
+  const entryProducts = entry.getIn(['data', 'intro', 'books']);
+  const products = entryProducts ? entryProducts.toJS() : [];
+  console.dir(entry.getIn(['data', 'intro', 'books']))
+  console.dir(entry.getIn(['data']))
+
+  return (
+    <ResourcesPageTemplate
+      image={getAsset(entry.getIn(['data', 'image']))}
+      title={entry.getIn(['data', 'title'])}
+      heading={entry.getIn(['data', 'heading'])}
+      description={entry.getIn(['data', 'description'])}
+      intro={entry.getIn(['data', 'introduction'])}
+      books={{books}}
+      products={{products}}
+    />
+  )
+}
+
+ResourcesPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+}
+
+export default ResourcesPagePreview
---
+++ src/pages/resources/index.md
@@ -0,0 +1,25 @@
+---
+templateKey: resources-page
+title: Our Coffee
+image: /img/jumbotron.jpg
+heading: Great coffee with a conscience
+description: >-
+  Kaldi is the ultimate spot for coffee lovers who want to learn about their
+  java’s origin and support the farmers that grew it. We take coffee production,
+  roasting and brewing seriously and we’re glad to pass that knowledge to
+  anyone.
+introduction: Testing jim meehanmd introduction
+books:
+    heading: testing heading
+    productImage: /img/jumbotron.jpg
+    category: NUTRITION
+    description: book description
+    url: https://google.com
+products:
+    heading: testing heading
+    productImage: /img/jumbotron.jpg
+    category: nutrition 
+    description: product description
+    url: https://google.com
+---
+
