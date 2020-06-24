5a7eacfa57a152a4e29f05401537df782a318f4a
Brian Coates <brian.coates@life.church>
2020-06-24 08:08:00 -0500

    absolute url for meta url
    Signed-off-by: Brian Coates <brian.coates@life.church>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -58,11 +58,11 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="https://meehanmd.com" />
         <meta
           property="og:image"
-          content={`${withPrefix}img/md-facebook.jpg`}
+          content='https://meehanmd.com/img/md-facebook.jpg'
         />
 
         <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:image" content={`${withPrefix('/')}img/md-twitter.jpg`}/>
+        <meta property="twitter:image" content='https://meehanmd.com/img/md-twitter.jpg'/>
 
         <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5e6d0baec4717a00120d076a&product=inline-share-buttons' async='async'></script>
 
